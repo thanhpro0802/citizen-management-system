12431557ab26e11b112cab2a2bca58195576bd0a
Nguyễn Tuấn Thành <thanh.nt235833@sis.hust.edu.vn>
2025-11-03 09:38:41 +0700

    Initialize backend repository with Maven setup and Spring Boot application

---
+++ src/test/java/com/citizen/management/citizen_management_system_back_end/CitizenManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizen.management.citizen_management_system_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
